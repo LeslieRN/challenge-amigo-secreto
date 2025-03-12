2198a5ba34ba7742e5a3616f8c016e7ad4f6041b
LeslieRN <lesliern30@hotmail.com>
2025-03-12 13:30:18 -0400

    Created logic for adding and sorting friends

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let inputText = document.getElementById("amigo");
+  if (inputText.value === "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    amigos.push(inputText.value);
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML += `<li>${inputText.value}</li>`;
+    inputText.value = "";
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length !== 0) {
+    let randomFriend = Math.floor(Math.random() * amigos.length);
+    let friend = amigos[randomFriend];
+    document.getElementById("listaAmigos").innerHTML = '';
+    document.getElementById(
+      "resultado"
+    ).innerHTML = `<li>El amigo secreto es: ${friend}</li>`;
+  }
+}
